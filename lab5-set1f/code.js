80df5b15421bb93018bea761cc682a0c12f9ecc7
Nabil M. Al-Rousan <rousani@gmail.com>
2022-05-24 14:53:56 -0800

    lab 5 - complete

--- lab5-set1f/code.js
+++ lab5-set1f/code.js
@@ -1,7 +1,48 @@
-function setup(){
-    $(".card").on("click", function (){
+secondCard = undefined
+firstCard = undefined
+
+cardHasBeenFlipped = false
+
+function setup() {
+    $(".card").on("click", function () {
         $(this).toggleClass("flip")
-    } ) 
+
+        if (!cardHasBeenFlipped) {
+            //captured first card
+            firstCard = $(this).find(".front_face")[0]
+            // console.log(firstCard);
+            cardHasBeenFlipped = true
+        } else {
+            secondCard = $(this).find(".front_face")[0]
+            console.log(firstCard, secondCard);
+            cardHasBeenFlipped = false;
+
+            // check if you have match
+            if (
+                $(`#${firstCard.id}`).attr("src")
+                ==
+                $(`#${secondCard.id}`).attr("src")
+            ) {
+                console.log("A Match!");
+                $(`#${firstCard.id}`).parent().off("click")
+                $(`#${secondCard.id}`).parent().off("click")
+            } else {
+                console.log("not a Match!");
+                setTimeout(() => {
+                    $(`#${firstCard.id}`).parent().removeClass("flip")
+                    $(`#${secondCard.id}`).parent().removeClass("flip")
+                }, 1000)
+
+            }
+        }
+
+
+
+        // reset 
+        // firstCard = undefined
+        // secondCard = undefined
+
+    })
 }
 
 
